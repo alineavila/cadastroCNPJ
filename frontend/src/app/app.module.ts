b6e56c320fc58716b2defa83dac5328ee6489ef4
alineavila <aline.avila01@gmail.com>
2020-02-16 16:09:46 -0300

    criando componentes de cabeçalho e rodapé

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ListaEmpresaComponent } from './lista-empresa/lista-empresa.component';
 import { FormEmpresaComponent } from './form-empresa/form-empresa.component';
 import { ErroComponent } from './erro/erro.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaEmpresaComponent,
     FormEmpresaComponent,
-    ErroComponent
+    ErroComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
