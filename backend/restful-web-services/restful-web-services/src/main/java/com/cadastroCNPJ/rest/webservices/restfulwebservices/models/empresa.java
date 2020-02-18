99f11ec95982cfcfbf1798587f8cd61d12c58232
alineavila <aline.avila01@gmail.com>
2020-02-18 12:52:12 -0300

    criando entidade Empresa

---
+++ backend/restful-web-services/restful-web-services/src/main/java/com/cadastroCNPJ/rest/webservices/restfulwebservices/models/empresa.java
@@ -0,0 +1,86 @@
+package com.cadastroCNPJ.rest.webservices.restfulwebservices.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_EMPRESA ")
+public class empresa implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String CNPJ;
+	private String nomeEmpresa;
+	private String tipoEmpresa;
+	private String razaoSocial;
+	private String contatoEmpresa;
+	private String emailEmpresa;
+	private long CEP;
+	private String UF;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCNPJ() {
+		return CNPJ;
+	}
+	public void setCNPJ(String cNPJ) {
+		CNPJ = cNPJ;
+	}
+	public String getNomeEmpresa() {
+		return nomeEmpresa;
+	}
+	public void setNomeEmpresa(String nomeEmpresa) {
+		this.nomeEmpresa = nomeEmpresa;
+	}
+	public String getTipoEmpresa() {
+		return tipoEmpresa;
+	}
+	public void setTipoEmpresa(String tipoEmpresa) {
+		this.tipoEmpresa = tipoEmpresa;
+	}
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getContatoEmpresa() {
+		return contatoEmpresa;
+	}
+	public void setContatoEmpresa(String contatoEmpresa) {
+		this.contatoEmpresa = contatoEmpresa;
+	}
+	public String getEmailEmpresa() {
+		return emailEmpresa;
+	}
+	public void setEmailEmpresa(String emailEmpresa) {
+		this.emailEmpresa = emailEmpresa;
+	}
+	public long getCEP() {
+		return CEP;
+	}
+	public void setCEP(long cEP) {
+		CEP = cEP;
+	}
+	public String getUF() {
+		return UF;
+	}
+	public void setUF(String uF) {
+		UF = uF;
+	}
+	
+	
+	
+
+}
