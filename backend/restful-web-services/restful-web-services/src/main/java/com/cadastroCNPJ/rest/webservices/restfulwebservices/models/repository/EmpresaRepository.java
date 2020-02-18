5239bca9ce8fe597eee97bbf2252c9a1af15fe03
alineavila <aline.avila01@gmail.com>
2020-02-18 17:13:09 -0300

    criando método POST e populando a tabela

--- backend/restful-web-services/restful-web-services/src/main/java/com/cadastroCNPJ/rest/webservices/restfulwebservices/models/repository/EmpresaRepository.java
+++ backend/restful-web-services/restful-web-services/src/main/java/com/cadastroCNPJ/rest/webservices/restfulwebservices/models/repository/EmpresaRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cadastroCNPJ.rest.webservices.restfulwebservices.models.empresa;
 
 public interface EmpresaRepository extends JpaRepository<empresa, Long>{
+	
+	empresa findById(long id);
 
 }
--- backend/restful-web-services/restful-web-services/src/main/java/com/cadastroCNPJ/rest/webservices/restfulwebservices/resources/EmpresaResource.java
+++ backend/restful-web-services/restful-web-services/src/main/java/com/cadastroCNPJ/rest/webservices/restfulwebservices/resources/EmpresaResource.java
@@ -2,6 +2,9 @@ package com.cadastroCNPJ.rest.webservices.restfulwebservices.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +25,14 @@ public class EmpresaResource {
 		return empresaRepository.findAll();
 	}
 	
+	@GetMapping("/empresa/{id}")
+	public empresa listaEmpresaUnica(@PathVariable(value = "id") long id){
+		return empresaRepository.findById(id);
+	}
+	
 
+	@PostMapping("/empresa")
+	public empresa salvaEmpresa(@RequestBody empresa empresa) {
+		return empresaRepository.save(empresa);
+	}
 }
