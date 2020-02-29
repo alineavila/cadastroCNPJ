edcc14175e9284da5e7eb948ad8f53938c33bd48
alineavila <aline.avila01@gmail.com>
2020-02-29 19:27:19 -0300

    criando camada de serviços e controller

--- backend/cadastroCnpj/cadastroCnpj/src/main/java/com/aline/cadastroCnpj/EmpresaServiceImp.java
+++ backend/cadastroCnpj/cadastroCnpj/src/main/java/com/aline/cadastroCnpj/EmpresaServiceImp.java
@@ -1,5 +1,46 @@
 package com.aline.cadastroCnpj;
 
-public class EmpresaServiceImp {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+
+public class EmpresaServiceImp implements EmpresaService{
+	@Autowired
+	private EmpresaRepository repositorio;
+	
+	
+	@Override
+	public List<Empresa> listar() {
+		return repositorio.findAll();
+	}
+
+	@Override
+	public Empresa listarId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Empresa add(Empresa empresa) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Empresa edit(Empresa empresa) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Empresa delete(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
 }
