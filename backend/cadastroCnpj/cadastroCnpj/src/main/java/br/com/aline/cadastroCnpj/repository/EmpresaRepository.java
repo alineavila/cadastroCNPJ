ce3e29a1cb6dcb5e69897e560600f7f8bbf98e39
alineavila <aline.avila01@gmail.com>
2020-03-01 16:44:42 -0300

    recriando o Repository

--- backend/cadastroCnpj/cadastroCnpj/src/main/java/br/com/aline/cadastroCnpj/repository/EmpresaRepository.java
+++ backend/cadastroCnpj/cadastroCnpj/src/main/java/br/com/aline/cadastroCnpj/repository/EmpresaRepository.java
@@ -1,5 +1,17 @@
 package br.com.aline.cadastroCnpj.repository;
 
-public class EmpresaRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.aline.cadastroCnpj.entity.Empresa;
+
+@Repository
+public interface EmpresaRepository  extends JpaRepository<Empresa, Long> {
+	Optional<Empresa> findByNome(String nome);
+	boolean existsByNome(String nome);
+	
+	
 
 }
--- backend/cadastroCnpj/cadastroCnpj/src/main/java/br/com/aline/cadastroCnpj/service/EmpresaService.java
+++ backend/cadastroCnpj/cadastroCnpj/src/main/java/br/com/aline/cadastroCnpj/service/EmpresaService.java
@@ -1,5 +1,17 @@
 package br.com.aline.cadastroCnpj.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.aline.cadastroCnpj.repository.EmpresaRepository;
+
+@Service
+@Transactional
 public class EmpresaService {
+	
+	@Autowired
+	EmpresaRepository empresaRepository;
 
 }
