1601d56de1e195a40c2d71cff15ce6f85d610b2e
alineavila <aline.avila01@gmail.com>
2020-03-01 17:04:41 -0300

    Criando EmpresaDTO

--- backend/cadastroCnpj/cadastroCnpj/src/main/java/br/com/aline/cadastroCnpj/dto/EmpresaDto.java
+++ backend/cadastroCnpj/cadastroCnpj/src/main/java/br/com/aline/cadastroCnpj/dto/EmpresaDto.java
@@ -1,5 +1,117 @@
 package br.com.aline.cadastroCnpj.dto;
 
+import javax.validation.constraints.NotBlank;
+
 public class EmpresaDto {
+
+	@NotBlank
+	private Long id;
+	@NotBlank
+	private String cnpj;
+	@NotBlank
+	private String tipo;
+	@NotBlank
+	private String nome;
+	@NotBlank
+	private String razao_social;
+	@NotBlank
+	private String email;
+	@NotBlank
+	private String contato;
+	@NotBlank
+	private Long cep;
+	@NotBlank
+	private String uf;
+	
+
+	public EmpresaDto(Long id, String cnpj, String tipo, String nome, String razao_social, String email, String contato,
+			Long cep, String uf) {
+		super();
+		this.id = id;
+		this.cnpj = cnpj;
+		this.tipo = tipo;
+		this.nome = nome;
+		this.razao_social = razao_social;
+		this.email = email;
+		this.contato = contato;
+		this.cep = cep;
+		this.uf = uf;
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRazao_social() {
+		return razao_social;
+	}
+
+	public void setRazao_social(String razao_social) {
+		this.razao_social = razao_social;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContato() {
+		return contato;
+	}
+
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
+
+	public Long getCep() {
+		return cep;
+	}
+
+	public void setCep(Long cep) {
+		this.cep = cep;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	
+	
 
 }
--- backend/cadastroCnpj/cadastroCnpj/src/main/java/br/com/aline/cadastroCnpj/service/EmpresaService.java
+++ backend/cadastroCnpj/cadastroCnpj/src/main/java/br/com/aline/cadastroCnpj/service/EmpresaService.java
@@ -1,10 +1,14 @@
 package br.com.aline.cadastroCnpj.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.aline.cadastroCnpj.entity.Empresa;
 import br.com.aline.cadastroCnpj.repository.EmpresaRepository;
 
 @Service
@@ -13,5 +17,37 @@ public class EmpresaService {
 	
 	@Autowired
 	EmpresaRepository empresaRepository;
+	
+	public List<Empresa> list(){
+		return empresaRepository.findAll();
+	}
+	
+	public Optional<Empresa> getOne(long id){
+		return empresaRepository.findById(id);
+		
+	}
+	
+	public Optional<Empresa> getByNome(String nome){
+		return empresaRepository.findByNome(nome);
+	}
+	
+	public void save(Empresa empresa) {
+		empresaRepository.save(empresa);
+		
+	}
+	
+	public void delete (long id) {
+		empresaRepository.deleteById(id);
+		
+	}
+	
+	public boolean existsById(long id) {
+		return empresaRepository.existsById(id);
+	}
+	
+	public boolean existsByNome(String nome) {
+		return empresaRepository.existsByNome(nome);
+	}
+	
 
 }
