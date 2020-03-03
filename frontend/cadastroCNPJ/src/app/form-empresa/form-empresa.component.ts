f2b62de44ddcf59625cb257cede1393a2691418c
alineavila <aline.avila01@gmail.com>
2020-03-03 01:15:24 -0300

    criando a ação novaEmpresa()

--- frontend/cadastroCNPJ/src/app/form-empresa/form-empresa.component.ts
+++ frontend/cadastroCNPJ/src/app/form-empresa/form-empresa.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { EmpresaService } from '../service/data/empresa.service';
+import { Empresa } from '../lista-empresa/lista-empresa.component';
 
 @Component({
   selector: 'app-form-empresa',
@@ -8,15 +10,43 @@ import { Router } from '@angular/router';
 })
 export class FormEmpresaComponent implements OnInit {
 
-  constructor(private router: Router) { }
+   cnpj: string;
+   tipo: string;
+   nome: string;
+   razaoSocial: string;
+   contato: number;
+   email: string;
+   cep: number;
+   uf: string;
+  
+
+  
+  constructor(private router: Router, private empresaService: EmpresaService) { }
 
   ngOnInit(): void {
   }
 
   listaEmpresa(){
-    this.router.navigate([""])
+    this.router.navigate([""]);
 
   }
+
+  novaEmpresa(){
+    const empresa = new Empresa(this.cnpj,
+      this.tipo,
+      this.nome,
+      this.razaoSocial, 
+      this.contato,
+      this.email,
+      this.cep,
+      this.uf)
+    this.empresaService.save(empresa);
+    this.router.navigate([""]);
+
+
+  }
+ 
+
 
 
 }
--- frontend/cadastroCNPJ/src/app/lista-empresa/lista-empresa.component.ts
+++ frontend/cadastroCNPJ/src/app/lista-empresa/lista-empresa.component.ts
@@ -5,15 +5,37 @@ import { HelloWorldDataService } from '../service/data/hello-world-data.service'
 import { EmpresaService } from '../service/data/empresa.service';
 
 export class Empresa {
-    public id: number;
-    public cnpj: string;
-    public tipo: string;  
-    public nome: string;
-    public razaoSocial: string;
-    public contato: number;
-    public email: string;
-    public cep: number;
-    public uf: string;
+  public id?: number;
+  public cnpj: string;
+  public tipo: string;
+  public nome: string;
+  public razaoSocial: string;
+  public contato: number;
+  public email: string;
+  public cep: number;
+  public uf: string;
+
+
+  constructor(
+     cnpj: string,
+     tipo: string,
+     nome: string,
+     razaoSocial: string,
+     contato: number,
+     email: string,
+     cep: number,
+     uf: string) {
+
+      this.cnpj = cnpj;
+      this.tipo = tipo;
+      this.nome = nome;
+      this.razaoSocial = razaoSocial; 
+      this.contato = contato;
+      this.email  = email;
+      this.cep = cep;
+      this.uf = uf;
+
+  }
 }
 
 @Component({
@@ -24,7 +46,7 @@ export class Empresa {
 
 export class ListaEmpresaComponent implements OnInit {
 
-  empresas: Empresa[]=[];
+  empresas: Empresa[] = [];
 
 
 
@@ -33,12 +55,12 @@ export class ListaEmpresaComponent implements OnInit {
     private router: Router,
     private service: HelloWorldDataService) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.listaEmpresas();
   }
 
 
-  listaEmpresas(): void{
+  listaEmpresas(): void {
     this.empresaService.getEmpresas().subscribe(
       data => {
         this.empresas = data;
@@ -50,15 +72,15 @@ export class ListaEmpresaComponent implements OnInit {
 
   }
 
-  novaEmpresa(){
+  novaEmpresa() {
     this.router.navigate(["formularioNovo"])
 
   }
+
   editaEmpresa(){
     this.router.navigate(["formularioNovo"])
   }
 
 
-  
 
 }
